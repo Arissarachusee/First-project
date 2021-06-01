0ab0cf934467a7abee240aada570b765ff1276c5
ArissaraLorance <>
2021-06-01 15:28:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//I just gonna write anything and show me the result if it works //
+console.log("Hello World")
+console.log('Launch' + 'Code'); console.log('ahhaha')
+console.log('milk\napple\tmeat')
+console.log('Coding Rocks')
+console.log("Coding Rocks")
+console.log("Hello World!")
+console.log("Coding Rocks");
+// Unit 4//
+console.log(typeof"17")
+console.log(typeof 17);
+console.log(typeof"17")
+console.log(typeof "3.2");
+console.log(typeof '5.2')
+// unit 4 still how to use quote 1.single quote 2.double quote//
+console.log(typeof "aris's kitchen")
+console.log(typeof'frog "princes!"')
+//how to give  numbers spaces or separate //
+console.log(42000)
+console.log(42,000)
+
+//4.2 Type of Conversion Number)//
+
+console.log(Number("2345"))
+console.log(typeof Number("2345"))
+console.log(Number(17))
+console.log()
+//below code result nan//
+console.log(Number("-2.5นนบนา"))
+//4.2 Type of Conversion String)//
+console.log(String (25))
+console.log(String (123.45))
+console.log(typeof String (123.45))
+console.log();
+//4.2 check your inderstasnding//
+console.log (Number (33));
+console.log (Number ('three'))
+console.log (Number ('3 3'))
+console.log (Number ('3'))
+console.log()
+// 4.3 Varialble = ตัวแปล//
